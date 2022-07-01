8b2b9fe8056c5ffc15b5421874af7fd4f033e2c0
Adrienne Ackley ackley.adrienne@gmail.com <ackley.adrienne@gmail.com>
2022-07-01 10:15:46 -0500

    model and controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
