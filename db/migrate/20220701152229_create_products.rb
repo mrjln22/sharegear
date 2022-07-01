df1a7e833cd8a962ac7f87f8e2c7e6675d268b03
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-07-01 16:29:38 +0100

    created products model and controller

---
+++ db/migrate/20220701152229_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :category
+      t.boolean :available
+      t.string :image
+      t.text :description
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
