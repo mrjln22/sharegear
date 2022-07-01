0beb754839412ef3e0f5d5d317a9136a085573b8
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-07-01 17:04:24 +0100

    added route to root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
   devise_for :users
-  root to: 'home#index'
+  root to: 'users#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
