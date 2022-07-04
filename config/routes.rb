b5330c6011d032e11be01f56082aa6ca7a3d6f3c
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-07-04 13:52:52 +0100

    added seeds

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
   get 'products/show'
+  get 'products/index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,54 +16,22 @@ Product.destroy_all
 
 puts "Creating Adrienne the user..."
 
-User.create!({
-  first_name: "Adrienne",
-  last_name: "Ackley",
-  dob: Date.parse("01-01-2000"),
-  address: "5 America street",
-  password: "123456",
-  email: "adrienne@adrienne.com"
-})
+User.create!(first_name: "Adrienne", last_name: "Ackley", dob: Date.parse("01-01-2000"), address: "5 America street", password: "123456", email: "adrienne@adrienne.com")
 
 puts "Creating Marjolijn the user..."
-User.create!({
-  first_name: "Adrienne",
-  last_name: "Ackley",
-  dob: Date.parse("01-01-2000"),
-  address: "5 England street ",
-  password: "123456",
-  email:"marjolijn@marjolijn.com"
-})
+User.create!(first_name: "Adrienne", last_name: "Ackley", dob: Date.parse("01-01-2000"), address: "5 England street ", password: "123456", email:"marjolijn@marjolijn.com")
 
 puts "Finished!"
 
 puts "Creating products...."
 
-product_one = Product.new({
-  name: "Kayak",
-  category: "watersports",
-  available: "yes",
-  description: "This is a great Kayak",
-  price: 100
-})
+product_one = Product.new(name: "Kayak", category: "watersports", available: "yes", description: "This is a great Kayak", price: 100)
 product_one.save
 
-product_two = Product.new({
-  name: "Surfboard",
-  category: "watersports",
-  available: "yes",
-  description: "This is a great surfboard",
-  price: 200
-})
+product_two = Product.new(name: "Surfboard", category: "watersports", available: "yes", description: "This is a great surfboard", price: 200)
 product_two.save
 
-product_three = Product.new({
-  name: "bicycle",
-  category: "cycling",
-  available: "yes",
-  description: "This is a great bike",
-  price: 500
-})
+product_three = Product.new(name: "bicycle", category: "cycling", available: "yes", description: "This is a great bike", price: 500)
 product_three.save
 
 puts "Finished!"
