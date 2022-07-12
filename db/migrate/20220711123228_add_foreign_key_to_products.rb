5cda4c31aa6b012d10f2b2396ac3af5abc237582
Adrienne Ackley ackley.adrienne@gmail.com <ackley.adrienne@gmail.com>
2022-07-12 12:06:23 -0500

    still working

--- db/migrate/20220711123228_add_foreign_key_to_products.rb
+++ db/migrate/20220711123228_add_foreign_key_to_products.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyToProducts < ActiveRecord::Migration[6.1]
   def change
-    # add_reference :products, :user, add_foreign_key: true
+    add_reference :products, :user, add_foreign_key: true
   end
 end
