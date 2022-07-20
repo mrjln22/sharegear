b1d14141d7534005898a9a3e7e33df61c3093bdc
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-07-20 14:57:19 +0100

    added routes user/id/booking/products

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :products do
     resources :bookings
   end
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    resources :products
+    resources :bookings
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
