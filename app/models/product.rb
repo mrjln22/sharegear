b591acb61565d0ede8cbdb47a0a8f71f37be8768
Adrienne Ackley ackley.adrienne@gmail.com <ackley.adrienne@gmail.com>
2022-07-28 09:02:15 -0500

    errors fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :name, presence: true
   validates :category, presence: true
   has_one_attached :photo
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :products
+  has_many :products, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :bookings_as_owner, through: :products, source: :bookings
   validates :role, presence: true
