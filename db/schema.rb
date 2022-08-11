f486636ec663c5b6023aac94378e14e4603f8501
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-08-11 19:48:10 +0100

    delete migration

--- db/schema.rb
+++ db/schema.rb
@@ -64,8 +64,6 @@ ActiveRecord::Schema.define(version: 2022_08_04_193106) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
