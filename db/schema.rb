bee143d31171d0ce931f22ce926c94293835f4d1
Adrienne Ackley ackley.adrienne@gmail.com <ackley.adrienne@gmail.com>
2022-08-17 07:32:28 -0500

    push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_152909) do
+ActiveRecord::Schema.define(version: 2022_08_04_193106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,12 +61,11 @@ ActiveRecord::Schema.define(version: 2022_08_03_152909) do
     t.string "image"
     t.text "description"
     t.integer "price"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.float "latitude"
     t.float "longitude"
-    t.string "address"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -91,4 +90,5 @@ ActiveRecord::Schema.define(version: 2022_08_03_152909) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "products"
   add_foreign_key "bookings", "users"
+  add_foreign_key "products", "users"
 end
