7e0e77bfcc5db42ea1a9ba2d69c5b248c0c99fec
Adrienne Ackley ackley.adrienne@gmail.com <ackley.adrienne@gmail.com>
2022-08-23 18:24:50 -0500

    updated landing page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_17_194928) do
+ActiveRecord::Schema.define(version: 2022_08_23_182809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
